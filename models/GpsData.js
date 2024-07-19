4df556d48620696571729113f61146fa2c62d1e5
Yadhu Krishna M J <yadddhu@gmail.com>
2024-07-19 12:57:56 +0530

    Minor patch

--- models/GpsData.js
+++ models/GpsData.js
@@ -1,6 +1,6 @@
-const mongoose = require('mongoose');
+const mongoose = require('mongoose'); //Mongoose Module for MongoDB Connection
 
-const gpsSchema = new mongoose.Schema({
+const gpsSchema = new mongoose.Schema({ 
   latitude: Number,
   longitude: Number,
   timestamp: { type: Date, default: Date.now }
