38425d8808ac56bfcfcbcbe656a6905a79fc764d
Yadhu Krishna M J <yadddhu@gmail.com>
2024-07-19 16:46:41 +0530

    Added Comments

--- models/GpsData.js
+++ models/GpsData.js
@@ -1,9 +1,11 @@
 const mongoose = require('mongoose'); //Mongoose Module for MongoDB Connection
 
+//New Schema for gps data storage in MongoDB
+
 const gpsSchema = new mongoose.Schema({ 
-  latitude: Number,
+  latitude: Number,      
   longitude: Number,
-  timestamp: { type: Date, default: Date.now }
+  timestamp: { type: Date, default: Date.now }    //Timestamp to access most recent and past location data
 });
 
 module.exports = mongoose.model('GpsData', gpsSchema);
--- routes/gpsData.js
+++ routes/gpsData.js
@@ -1,13 +1,14 @@
-const express = require('express');
-const router = express.Router();
-const GpsData = require('../models/GpsData');
+const express = require('express');       //Express Module for creating express app
+const router = express.Router();        //Express Router for routing
+const GpsData = require('../models/GpsData');     //Gps data model in MONGODB
 
+//Handle POST method for ESP32 to send new location data to the MongoDB Atlas database(Local)
 router.post('/gps-data', async (req, res) => {
-  const { latitude, longitude } = req.body;
+  const { latitude, longitude } = req.body;     //Acquire latitude and longitute from request body
   const newGpsData = new GpsData({ latitude, longitude });
 
   try {
-    await newGpsData.save();
+    await newGpsData.save();      //Save the newly acquired data as a document
     res.status(200).send('Data saved successfully');
   } catch (err) {
     console.error(err);
@@ -15,10 +16,12 @@ router.post('/gps-data', async (req, res) => {
   }
 });
 
+
+//get method for the frontend to update the map with the latest location data
 router.get('/latest-gps-data', async (req, res) => {
   try {
-    const latestData = await GpsData.findOne().sort({ timestamp: -1 }).exec();
-    res.json(latestData);
+    const latestData = await GpsData.findOne().sort({ timestamp: -1 }).exec();      //Sort by timestamp to get latest location document
+    res.json(latestData);     
   } catch (err) {
     console.error(err);
     res.status(500).send(err);
@@ -26,3 +29,4 @@ router.get('/latest-gps-data', async (req, res) => {
 });
 
 module.exports = router;
+
