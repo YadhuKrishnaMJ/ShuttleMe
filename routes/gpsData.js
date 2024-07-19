5e20dcb80d20db25afc16a9d78a8f9e18828a9e9
Yadhu Krishna M J <yadddhu@gmail.com>
2024-07-19 12:06:07 +0530

    Changed tunnel from ngrok to Localtunnel.me

--- routes/gpsData.js
+++ routes/gpsData.js
@@ -2,27 +2,27 @@ const express = require('express');
 const router = express.Router();
 const GpsData = require('../models/GpsData');
 
-router.post('/gps-data', (req, res) => {
+router.post('/gps-data', async (req, res) => {
   const { latitude, longitude } = req.body;
   const newGpsData = new GpsData({ latitude, longitude });
 
-  newGpsData.save((err) => {
-    if (err) {
-      res.status(500).send(err);
-    } else {
-      res.status(200).send('Data saved successfully');
-    }
-  });
+  try {
+    await newGpsData.save();
+    res.status(200).send('Data saved successfully');
+  } catch (err) {
+    console.error(err);
+    res.status(500).send(err);
+  }
 });
 
-router.get('/latest-gps-data', (req, res) => {
-  GpsData.findOne().sort({ timestamp: -1 }).exec((err, data) => {
-    if (err) {
-      res.status(500).send(err);
-    } else {
-      res.json(data);
-    }
-  });
+router.get('/latest-gps-data', async (req, res) => {
+  try {
+    const latestData = await GpsData.findOne().sort({ timestamp: -1 }).exec();
+    res.json(latestData);
+  } catch (err) {
+    console.error(err);
+    res.status(500).send(err);
+  }
 });
 
 module.exports = router;
--- server.js
+++ server.js
@@ -1,19 +1,23 @@
-require('dotenv').config();
-const express = require('express');
-const bodyParser = require('body-parser');
-const mongoose = require('mongoose');
-const gpsDataRoutes = require('./routes/gpsData');
+//Required modules are imported
 
-const app = express();
-const port = process.env.PORT || 3000;
+require('dotenv').config();     //Dotenv for environment variables
+const express = require('express');     //Express app
+const bodyParser = require('body-parser');    //BodyParser middleware, Parse json objects etc
+const mongoose = require('mongoose');     //For Local MongoDB connection
+const gpsDataRoutes = require('./routes/gpsData');     //Contains all routes for gps api
 
-app.use(bodyParser.json());
-app.use(express.static('public'));
+const app = express();      //Create an Express App
+const port = process.env.PORT || 3000;    //Access environment variable port
 
-mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true });
+app.use(bodyParser.json());     //Parse incoming requests with json payload
+app.use(express.static('public'));    //Serve static pages in public
 
-app.use('/api', gpsDataRoutes);
+mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true });   //Connect with mongoDB, Options for new MongoDB Driver
 
-app.listen(port, () => {
+app.use('/api', gpsDataRoutes);   //Use the gps api routes
+
+
+//Listen in Port
+app.listen(port, () => {   
   console.log(`Server running at http://localhost:${port}/`);
 });
